0de36f7a0cb60ceb46a8f33ac266a331f9765f71
ken Rimple <krimple@chariotsolutions.com>
2020-08-10 18:06:30 -0400

    tweaks

--- setup-codebuild.sh
+++ setup-codebuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Usage:  ./setup-codebuild.sh project-stack-name githuburl
+# Usage:  ./setup-codebuild.sh angular-stack-name cypress-stack-name githuburl
 WEBSITE_URL=`aws cloudformation describe-stacks --stack-name ${1} --output text --query "Stacks[*].Outputs[?OutputKey=='WebsiteURL'].OutputValue"`
 BUCKET_NAME=`aws cloudformation describe-stacks --stack-name ${1} --output text --query "Stacks[*].Outputs[?OutputKey=='S3BucketName'].OutputValue"`
-aws cloudformation deploy --stack-name ${1} --capabilities CAPABILITY_IAM \
+aws cloudformation deploy --stack-name ${2} --capabilities CAPABILITY_IAM \
   --template-file ./cypress-tests/cloudformation.yml \
-  --parameter-overrides CypressBaseUrl=${WEBSITE_URL} GithubProjectUrl=${2}
+  --parameter-overrides CypressBaseUrl=${WEBSITE_URL} GithubProjectUrl=${3}
